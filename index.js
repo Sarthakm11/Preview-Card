afc91d985cb29205103f05b2437065d608ed574c
Sarthakm11 <85766914+Sarthakm11@users.noreply.github.com>
2021-07-26 04:04:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-// // for (var i=0; i<document.querySelector(".btn").classList.length; i++) {
-// //
-// //   document.querySelectorAll(".btn")[i].addEventListener("click",function()
-//
-//   function pressButton(){
-//     document.querySelector(".btn").classList.add("press");
-//     setTimeout(function(){
-//       document.querySelector(".btn").classList.remove("press");
-//     },3000);
-//
-//   });
-// }
+
